6661d19b7ce87237709ac80cf33a1f52db455135
ishan Sahu <ishan20015.s@gmail.com>
2024-06-16 09:43:49 +0530

    added basic responsiveness

--- index.js
+++ index.js
@@ -34,23 +34,22 @@ function submit_message() {
 // Function to initialize each carousel
 function initializeCarousel(carouselClass) {
     const carousel = document.querySelector(`.${carouselClass}`);
-    console.log(carouselClass, carousel)
     const rightButton = carousel.querySelector(`.arrow-right`);
     const leftButton = carousel.querySelector(`.arrow-left`);
     const cards = carousel.querySelector('.cards');
   
-    let totalPixels = 0;
-    const cardWidth = cards.querySelector(".node-card").getBoundingClientRect().width;
-    console.log(cardWidth)
+    let coverdPixels = 0;
+    const cardWidth = cards.querySelector(".node-card").getBoundingClientRect().width * 2;
     const visibleCardsCount = Math.floor(carousel.clientWidth / cardWidth);
     const totalCardsCount = cards.children.length;
+    console.log(cardWidth, totalCardsCount)
     const leftArrowSpans = carousel.querySelectorAll(".arrow-left span")
     const rightArrowSpans = carousel.querySelectorAll(".arrow-right span")
   
     rightButton.disabled = false;
   
     function updateButtonStates() {
-      if (totalPixels === 0) {
+      if (coverdPixels === 0) {
         leftButton.disabled = true;
         leftButton.style.opacity = '0.5';
         leftArrowSpans.forEach(span => {
@@ -66,7 +65,7 @@ function initializeCarousel(carouselClass) {
       }
   
       const maxMove = (totalCardsCount - visibleCardsCount) * cardWidth;
-      if (Math.abs(totalPixels) >= maxMove) {
+      if (Math.abs(coverdPixels) >= maxMove) {
         rightButton.disabled = true;
         rightButton.style.opacity = '0.5';
         rightArrowSpans.forEach(span => {
@@ -85,17 +84,19 @@ function initializeCarousel(carouselClass) {
   
     function moveToRight() {
       const maxMove = (totalCardsCount - visibleCardsCount) * cardWidth;
-      if (Math.abs(totalPixels) < maxMove) {
-        totalPixels -= cardWidth;
-        cards.style.transform = `translateX(${totalPixels}px)`;
+      if (Math.abs(coverdPixels) < maxMove) {
+        coverdPixels -= cardWidth;
+        cards.style.transform = `translateX(${coverdPixels}px)`;
+        console.log(coverdPixels, maxMove)
         updateButtonStates();
       }
     }
   
     function moveToLeft() {
-      if (totalPixels < 0) {
-        totalPixels += cardWidth;
-        cards.style.transform = `translateX(${totalPixels}px)`;
+      if (coverdPixels < 0) {
+        coverdPixels += cardWidth;
+        cards.style.transform = `translateX(${coverdPixels}px)`;
+        console.log(coverdPixels)
         updateButtonStates();
       }
     }
